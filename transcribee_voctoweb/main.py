55d6aceb080cc5f190e8dc4ae5c7c5d5a4b4e138
Philipp Mandler <info@philipp-mandler.com>
2023-12-13 16:55:40 +0100

    Replace deprecated startup event

--- transcribee_voctoweb/main.py
+++ transcribee_voctoweb/main.py
@@ -1,4 +1,6 @@
 import asyncio
+from contextlib import asynccontextmanager
+import logging
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
@@ -9,29 +11,37 @@ import requests
 from transcribee_voctoweb.config import settings
 from transcribee_voctoweb.helpers.periodic_tasks import run_periodic
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    update_conference()
+    asyncio.create_task(run_periodic(update_conference, seconds=15))
 
+    yield
+
+    # shutdown
+    # ...
+
+
+app = FastAPI(lifespan=lifespan)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-conference = requests.get("https://api.media.ccc.de/public/conferences/35c3").json()
-events = conference["events"]
+def update_conference():
+    logging.debug("Updating conference...")
+    global conference
+    conference = requests.get("https://api.media.ccc.de/public/conferences/35c3").json()
+    global events
+    events = conference["events"]
+
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request, "title": "Some title", "events": events})
+    return templates.TemplateResponse("home.html", {"request": request, "events": events})
 
 
 @app.get("/events/{id}", response_class=HTMLResponse)
 async def event(request: Request, id: str):
     event = next((event for event in events if event["guid"] == id), None)
-    return templates.TemplateResponse("event.html", { "request": request, "title": "Some title", "event": event})
-
-
-def periodic_task():
-    print("periodic task")
-
-@app.on_event("startup")
-async def setup_periodic_tasks():
-    asyncio.create_task(run_periodic(periodic_task, seconds=10))
+    return templates.TemplateResponse("event.html", { "request": request, "event": event})
